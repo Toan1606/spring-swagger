69a3ad4013d3be2accdf11334154050029728795
Toan1606 <nguyenthetoan1606@gmail.com>
2022-11-15 23:29:50 +0700

    edit config swagger

--- src/main/java/com/technique/config/SwaggerConfig.java
+++ src/main/java/com/technique/config/SwaggerConfig.java
@@ -16,8 +16,12 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.basePackage("com.technique"))
+                .build();
+        /*return new Docket(DocumentationType.SWAGGER_2)
+                .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build();*/
     }
 }
